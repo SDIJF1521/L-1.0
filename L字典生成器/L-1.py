193dff05707d8439d6ae5e3b1b7cfe50047ac01a
SDIJF1521 <69745333+SDIJF1521@users.noreply.github.com>
2022-03-19 00:17:43 +0800

    Update L-1.py

--- L字典生成器/L-1.py
+++ L字典生成器/L-1.py
@@ -2,38 +2,41 @@
 while len(文件) == False:
     print('文件名称不能为空')
     文件 = input("请定义文件名称：")
-number1 = input('请输入最小密码数值：')
-C = number1
-number1 = int(number1)
-while number1 < -1:
-    print('最小密码数值必须大于等于0')
-    number1 = input('请输入最小密码数值：')
-    C = number1
-    number1 = int(number1)
-step = int(input('请设置步长：'))
-while step < 1:
-    print('步长必须大于等于1')
-    step = int(input('请设置步长：'))
-number2 = int(input('请输入最大密码数值：'))+1
-while number2 < number1:
-    print('最大密码数值不能小于最小密码数值')
-    numer2 = int(input('请输入最大密码数值：'))+1
-file = open(文件, 'w')
-for i in range(number1,number2,step):     #生成原始密码
-    mima= str(i)
-    mima1 = list(mima)
-    ZD = []
-    ZD1=['0']
-    CD1 = int(len(mima1))
-    z = list(C)
-    CD2 = int(len(z))
-    CD = CD2 - CD1
-    while CD != 0:                 #补全前端0位
-        ZD.extend(ZD1[0])
-        CD2 = len(ZD)
-        CD = CD-1
-    ZD.extend(mima1)              #生成密码
-    Y = ''.join(ZD)               #去除多余符号
-    print(Y)
-    file.write(Y)  # 写入密码      #存入字典文件
-    file.write("\r")
+A1 = input("请输入最小密码数值：")            #定义最小密码数值
+A = int(A1)
+while A <0 :                                   #判断密码长度是否大于等于0
+    print("最小数值必须大于等于0")
+    A1 = input("请输入最小密码数值：")
+    A = int(A1)
+B1 = input("请输入最大密码数值：")            #定义最大密码数值
+B = int(B1)
+while B<A :
+    print("数值必须大于或小于最小数值")        #判断最大密码数值是否大于或者等于最小密码数值
+    B1 = input("请输入最大密码数值")
+    B = int(B1)
+file = open(文件,'w')
+C=str(A1)
+file.write(C)                                 
+file.write('\r')                         
+A2 = list(A1)                                
+while A < B :                               
+    A = A + 1                                 
+    C=str(A)                                
+    C1 = list(C)                             
+    CD = len(C1)                              
+    CD1 = len(A2)                             
+    CD1 = int(CD1)
+    LB = []
+    LB1 = ['0']
+    CD2 = len(LB)
+    CD2 = int(CD2)
+    CD3 = CD1 -CD
+    while CD3  > CD2 :
+        LB.extend(LB1[0])
+        CD2 = len(LB)
+        CD = list(C)
+    LB.extend(C)
+    C = ''.join(LB)
+    print(C)
+    file.write(C)                             #写入密码
+    file.write("\r")                        #换行
